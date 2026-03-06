8868f06841a519d05dd9688a87e4bb4f58a882c4
shibin.antony@ospyn.com <shibin.antony@ospyn.com>
2026-03-06 11:04:01 +0530

    jest issue fixed

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,8 +6,16 @@ import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
+jest.mock('../src/api/ListService', () => ({
+  getProducts: jest.fn(() =>
+    Promise.resolve({ products: [], total: 0, skip: 0, limit: 10 })
+  ),
+}));
+
 test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
+  await ReactTestRenderer.act(async () => {
     ReactTestRenderer.create(<App />);
+    await Promise.resolve();
+    await Promise.resolve();
   });
 });
